d680d4edbf564a3290fd0faf233f624a35422128
Prakash Bhattarai <lightvelocity999@gmail.com>
2023-12-30 08:58:33 +0545

    Initial Commit

---
+++ src/test/java/com/mrbpurnachandra/jourvicebackend/JourviceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbpurnachandra.jourvicebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourviceBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
